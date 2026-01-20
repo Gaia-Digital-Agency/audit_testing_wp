07f85b34df6979f066cd5bacf08f4453b6058756
azlangaiada <azlan22@proton.me>
2026-01-20 18:29:44 +0800

    small changes 14

--- homepage.spec.js
+++ homepage.spec.js
@@ -3,6 +3,6 @@ const { test, expect } = require("@playwright/test");
 test.describe("Homepage", () => {
   test("should have the correct title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle(/Twenty Twenty-Four/);
+    await expect(page).toHaveTitle(/Twenty Twenty-Five/);
   });
 });
